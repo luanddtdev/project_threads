01e5ef36878b62f1774432bc5aa866bf40bfd309
LDS <luanddt.dev@gmail.com>
2024-10-19 21:30:40 +0700

    update home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,24 @@
+import Bottombar from "@/components/shared/Bottombar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+import Topbar from "@/components/shared/Topbar"
+
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            {children}
+            <Topbar />
+
+            <div className="flex">
+                <LeftSidebar />
+
+                <div className="max-w-4xl mx-auto min-h-screen pt-28 sm:px-10 px-6 md:pb-10 pb-32 flex-1">
+                    {children}
+                </div>
+
+                <RightSidebar />
+            </div>
+
+            <Bottombar />
         </>
     )
 }
